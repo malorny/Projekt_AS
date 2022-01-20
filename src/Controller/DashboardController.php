196c6243b6300aee87a51fa3576d2295e894a391
malorny <dawid.malorny@poczta.fm>
2022-01-20 14:11:05 +0100

    Tabela z sortowaniem i wyszukiwaniem dla użytkownika

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Entity\Reserve;
 use App\Entity\Lake;
@@ -15,26 +16,36 @@ class DashboardController extends AbstractController
 {
     public function index(Request $request, ManagerRegistry $mr)
     {
+        return $this->render('dashboard.html.twig', []);
+    }
+
+    public function getReservations(Request $request, ManagerRegistry $mr): JsonResponse
+    {
         $reserveRepo = $mr->getManager()->getRepository(Reserve::class);
         $lakeRepo = $mr->getManager()->getRepository(Lake::class);
 
         $userReservationsRaw = $reserveRepo->findBy(['user_id' => $this->getUser()->getId()]);
         $userReservations = [];
+        $total = 0;
+
+        $userReservations['rows'] = [];
 
         foreach ($userReservationsRaw as $raw) {
             $reservation = [];
 
-            $reservation['begin'] = $raw->getBeginDate();
-            $reservation['end'] = $raw->getEndDate();
+            $reservation['id'] = $raw->getId();
+            $reservation['begin'] = $raw->getBeginDate()->format("d/m/Y h:i:s A");
+            $reservation['end'] = $raw->getEndDate()->format("d/m/Y h:i:s A");
             $reservation['name'] = $lakeRepo->findOneBy(['id' => $raw->getLakeId()])->getName();
 
-            $userReservations[] = $reservation;
+            $userReservations['rows'][] = $reservation;
+
+            $total++;
         }
 
-        // var_dump($userReservations);
+        $userReservations['total'] = $total;
+        $userReservations['totalNotFiltered'] = $total;
 
-        return $this->render('dashboard.html.twig', [
-            'userReservations' => $userReservations
-        ]);
+        return new JsonResponse($userReservations);
     }
 }
